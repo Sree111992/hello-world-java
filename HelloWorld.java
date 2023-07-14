d6842f5239fec6d81498c984d323fa088b2a214b
Sree111992 <138361039+Sree111992@users.noreply.github.com>
2023-07-14 23:53:23 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("Welcome you all to");
         System.out.println("Harsha DevOps Tutorials"); 
         // Hello World!
     }
