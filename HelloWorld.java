14194a929b4d7a2aa91f25979b83125c962fb95e
Sree111992 <138361039+Sree111992@users.noreply.github.com>
2023-07-14 23:39:21 +0530

    new HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello world! Good day!! Thank you for the session!!!");
-  }
+public class MyFirstJavaProgram {
+
+   /* This is my first java program.
+    * This will print 'Hello World' as the output
+    */
+
+   public static void main(String []args) {
+      System.out.println("Welcome to HARSHA REAL TIME DEVOPS TUTORIALS");
+   }
 }
