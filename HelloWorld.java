7cbc71527dcf8a88d6fdad6a666a9648520856f4
Sree111992 <138361039+Sree111992@users.noreply.github.com>
2023-07-14 23:35:37 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world! Good day!!");
-      System.out.println("Thank you for the session");
+    System.out.println("Hello world! Good day!! Thank you for the session!!!");
   }
 }
