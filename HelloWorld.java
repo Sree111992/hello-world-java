a84061f656febae97fe37941550908138c3be844
Sree111992 <138361039+Sree111992@users.noreply.github.com>
2023-07-27 21:57:58 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,9 @@ class Test
     public static void main(String []args)
     {
         System.out.println("My First DevOps class.");
+        System.out.println("My First DevOps class.");
+        System.out.println("My First DevOps class.");
+        System.out.println("My First DevOps class.");
+        System.out.println("My First DevOps class.");
     }
 };
